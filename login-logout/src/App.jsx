72b3e0c492ce58814c84bf9c23434cfabb0cf5d2
natai nakata <satzin0521@gmail.com>
2024-09-02 16:00:12 +0900

    complete login-logout

--- login-logout/src/App.jsx
+++ login-logout/src/App.jsx
@@ -3,9 +3,32 @@ import "./App.css";
 
 function App() {
   const [isLoggedIn, setLoggedIn] = useState(false);
+  const handleLogin = () => {
+    setLoggedIn(true);
+  };
+  const handleLogout = () => {
+    setLoggedIn(false);
+  };
   const element = (
     <div className="container">
-      <div className="content"></div>
+      <div className="content">
+        {isLoggedIn ? (
+          <div>
+            <h1 className="title">ログイン済みです</h1>
+            <button className="button" onClick={handleLogout}>
+              ログアウト
+            </button>
+          </div>
+        ) : (
+          <div>
+            <h1 className="title">ログインしてください</h1>
+            <button className="button" onClick={handleLogin}>
+              ログイン
+            </button>
+          </div>
+        )}
+        <span>isLoggedIn: {isLoggedIn.toString()}</span>
+      </div>
     </div>
   );
   return element;
