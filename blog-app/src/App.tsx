3c51f500537bcca044f43483cd1307c5b72678a0
natai nakata <satzin0521@gmail.com>
2024-10-14 11:57:14 +0900

    complete blog-app

--- blog-app/src/App.tsx
+++ blog-app/src/App.tsx
@@ -1,7 +1,23 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import "./App.css";
+import Header from "./components/Header";
+import Home from "./components/Home";
+import About from "./components/About";
+import Blog from "./components/Blog";
 
 function App() {
-  return <></>;
+  return (
+    <div>
+      <BrowserRouter>
+        <Header />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/blog" element={<Blog />}/>
+        </Routes>
+      </BrowserRouter>
+    </div>
+  )
 }
 
 export default App;
